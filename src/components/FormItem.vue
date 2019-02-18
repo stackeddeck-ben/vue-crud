2409264342f6d93baab12048b0fdcf5be7e56415
korakot-ben <34016384+stackeddeck-ben@users.noreply.github.com>
2019-02-18 15:19:06 +0700

    Home and FormItem Page

--- src/components/FormItem.vue
+++ src/components/FormItem.vue
@@ -18,7 +18,7 @@
       <div class="form-group">
         <label class="col-md-4 control-label" for="thumbnailUrl">Thumbnail URL</label>
         <div class="col-md-4">
-        <input id="thumbnailUrl" name="thumbnailUrl" type="text" placeholder="thumbnail url" class="form-control input-md" required="" >
+        <input id="thumbnailUrl" name="thumbnailUrl" type="text" placeholder="thumbnail url" class="form-control input-md" required="" v-model="thumbnailUrl">
 
         </div>
       </div>
@@ -27,7 +27,7 @@
       <div class="form-group">
         <label class="col-md-4 control-label" for="url">URL</label>
         <div class="col-md-4">
-        <input id="url" name="url" type="text" placeholder="url" class="form-control input-md">
+        <input id="url" name="url" type="text" placeholder="url" class="form-control input-md" v-model="url">
 
         </div>
       </div>
@@ -37,9 +37,38 @@
         <label class="col-md-4 control-label" for="save"></label>
         <div class="col-md-8">
           <button id="save" name="save" class="btn btn-success">Save</button>
-          <button id="cancel" name="cancel" class="btn btn-danger">Cancel</button>
+          <button id="cancel" name="cancel" class="btn btn-danger" @click="backToHome">Cancel</button>
         </div>
       </div>
     </fieldset>
   </form>
 </template>
+
+<script>
+  const API_URL = 'http://localhost:5000/photos'
+  export default{
+    name: 'FormItem',
+    data: function () {
+      return {
+        id: null,
+        title: null,
+        url: null,
+        thumbnailUrl: null
+      }
+    },
+    methods: {
+      onSubmit: function () {
+        this.$http.post(API_URL,
+          {title: this.title, url: this.url, thumbnailUrl: this.thumbnailUrl}
+        ).then(response =>{
+          this.backToHome()
+        }, response => {
+          this.backToHome()
+        })
+      },
+      backToHome: function (){
+        this.$router.push({name: 'Home'})
+      }
+    }
+  }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
     </button>
     <br />
     <br />
-    <div class="col-sm-6 col-md-4">
+    <!-- <div class="col-sm-6 col-md-4">
       <div class="thumbnail">
         <img src="http://placehold.it/600/92c952" alt="accusamus beatae ad facilis cum similique qui sunt">
         <div class="caption">
@@ -28,6 +28,14 @@
         <img src="http://placehold.it/600/92c952" alt="accusamus beatae ad facilis cum similique qui sunt">
         <div class="caption">
           <h3>accusamus beatae ad facilis cum similique qui sunt</h3>
+        </div>
+      </div>
+    </div> -->
+    <div class="col-sm-6 col-md-4" v-for="item in list" :key="item.id">
+      <div class="thumbnail">
+        <img :src="item.url" :alt="item.title">
+        <div class="caption">
+          <h3>{{ item.title }}</h3>
         </div>
       </div>
     </div>
@@ -35,12 +43,32 @@
 </template>
 
 <script>
+
+const API_URL = 'http://localhost:5000/photos'
+
 export default {
   name: 'Home',
+  data: function () {
+    return {
+      list: []
+    }
+  },
   methods: {
     goAdd: function () {
       this.$router.push('/add')
+    },
+    loadDate: function () {
+      this.$http.get(API_URL)
+        .then(response => {
+          console.log(response)
+          this.list = response.body
+        }, () => {
+          this.list = []
+        })
     }
+  },
+  created: function () {
+    this.loadDate()
   }
 }
 </script>
